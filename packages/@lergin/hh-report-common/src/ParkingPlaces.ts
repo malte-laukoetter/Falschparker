8b635f40f0b0da813ac53e567e0cfdc29ecacc56
Lergin <lergin@gmx.de>
2019-02-14 20:53:23 +0100

    Add not parking at the edge of the street as new place

--- packages/@lergin/hh-report-common/src/ParkingPlaces.ts
+++ packages/@lergin/hh-report-common/src/ParkingPlaces.ts
@@ -2,5 +2,6 @@ export enum ParkingPlaces {
   BICYCLE_OR_FOOD_PATH = 'Geh-/Radweg',
   BICYCLE_PROTECTION_LANE = 'Schutzstreifen',
   INTERSECTION = 'Kreuzung',
+  NOT_STREET_EDGE = 'nicht am Straßenrand',
   EMERGENCY_ACCESS_ROAD = 'Feuerwehrzufahrt'
 }
