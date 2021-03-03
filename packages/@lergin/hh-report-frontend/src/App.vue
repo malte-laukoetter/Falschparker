335d7e78ce77212153c871158294aa6ccec42d03
Lergin <malte@laukoetter.eu>
2021-03-03 11:17:59 +0100

    Fix icons

--- packages/@lergin/hh-report-frontend/src/App.vue
+++ packages/@lergin/hh-report-frontend/src/App.vue
@@ -11,8 +11,8 @@
        <v-toolbar-items>
         <v-btn text @click="() => $router.push('/')"><v-icon>mdi-notebook-multiple</v-icon></v-btn>
         <v-btn icon @click="() => $router.push('/map')"><v-icon>mdi-map</v-icon></v-btn>
-        <v-btn icon @click="() => $router.push('/stats')"><v-icon>mdi-chart-box-outline</v-icon></v-btn>
-        <v-btn icon @click="() => $router.push('/settings')"><v-icon>mdi-gear</v-icon></v-btn>
+        <v-btn icon @click="() => $router.push('/stats')"><v-icon>mdi-poll-box</v-icon></v-btn>
+        <v-btn icon @click="() => $router.push('/settings')"><v-icon>mdi-cog</v-icon></v-btn>
         <v-spacer></v-spacer>
         <v-btn icon @click="logout()"><v-icon>mdi-logout-variant</v-icon></v-btn>
         <v-btn icon @click="dark = !dark"><v-icon>mdi-brightness-6</v-icon></v-btn>
