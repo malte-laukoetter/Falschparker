17305ba66fc3b20916145ad7c055fccfd0dd102e
Lergin <malte@laukoetter.eu>
2021-03-11 10:31:13 +0100

    Run functions deployment via github actions

--- netlify_build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-npm run build
-
-if git diff --quiet HEAD^ HEAD functions/; then
-  echo "No function changes, skip function deployment."
-else 
-  npm run deploy:functions
-fi
